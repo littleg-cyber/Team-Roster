ee37fb83dcf76e39e0c72701aeaf03c072dcc26d
Grace Little <gracevlittle@gmail.com>
2023-04-25 21:24:59 -0500

    finished create-read-delete

---
+++ components/forms/MemberForm.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../../utils/context/authContext';
+import { getHouses } from '../../api/houseData';
+import { createMember, updateMember } from '../../api/memberData';
+
+const initialState = {
+  image: '',
+  name: '',
+  role: false,
+};
+
+function MemberForm({ obj }) {
+  const [formInput, setFormInput] = useState(initialState);
+  const [houses, setHouses] = useState([]);
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    getHouses(user.uid).then(setHouses);
+
+    if (obj.firebaseKey) setFormInput(obj);
+  }, [obj, user]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firebaseKey) {
+      updateMember(formInput)
+        .then(() => router.push(`/member/${obj.firebaseKey}`));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createMember(payload).then(() => {
+        router.push('/');
+      });
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Member</h2>
+
+      {/* TITLE INPUT  */}
+      <FloatingLabel controlId="floatingInput1" label="Member Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter a name"
+          name="name"
+          value={formInput.title}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* IMAGE INPUT  */}
+      <FloatingLabel controlId="floatingInput2" label="Member Image" className="mb-3">
+        <Form.Control
+          type="url"
+          placeholder="Enter an image url"
+          name="image"
+          value={formInput.image}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+      {/* AUTHOR SELECT  */}
+      <FloatingLabel controlId="floatingSelect" label="House">
+        <Form.Select
+          aria-label="House"
+          name="house_id"
+          onChange={handleChange}
+          className="mb-3"
+          value={obj.house_id} // FIXME: modify code to remove error
+          required
+        >
+          <option value="">Select a House</option>
+          {
+            houses.map((house) => (
+              <option
+                key={house.firebaseKey}
+                value={house.firebaseKey}
+              >
+                {house.title}
+              </option>
+            ))
+          }
+        </Form.Select>
+      </FloatingLabel>
+
+      {/* SUBMIT BUTTON  */}
+      <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Member</Button>
+    </Form>
+  );
+}
+
+MemberForm.propTypes = {
+  obj: PropTypes.shape({
+    image: PropTypes.string,
+    name: PropTypes.string,
+    role: PropTypes.bool,
+    house_id: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }),
+};
+
+MemberForm.defaultProps = {
+  obj: initialState,
+};
+
+export default MemberForm;
